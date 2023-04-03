0a49de34d9bbb11882c303ea42f542fd9bfb89f2
Trenton <trenflem@gmail.com>
2023-04-03 00:57:10 -0400

    setup heartbeat

--- raft/go.mod
+++ raft/go.mod
@@ -1,13 +1,12 @@
-module github/twflem/dist-sys/raft
+module github.com/twflem/dist-sys/raft
 
 go 1.19
 
-require github.com/stretchr/testify v1.8.2
+require google.golang.org/protobuf v1.28.1
 
 require (
-	github.com/bufbuild/connect-go v1.5.2 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/bufbuild/connect-go v1.5.2
+	golang.org/x/net v0.8.0
 )
+
+require golang.org/x/text v0.8.0 // indirect
--- raft/main.go
+++ raft/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+
+	heartbeatv1 "github.com/twflem/dist-sys/raft/gen/proto/raft/v1"
+	"github.com/twflem/dist-sys/raft/gen/proto/raft/v1/heartbeatv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	connect "github.com/bufbuild/connect-go"
 )
+
+const address = "localhost:8080"
 
 func main() {
 	mux := http.NewServeMux()
+	path, handler := heartbeatv1connect.NewHeartbeatHandler(&HeartbeatService{})
+	mux.Handle(path, handler)
+	fmt.Println("... Listening on", address)
+	http.ListenAndServe(
+		address,
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
+
+type HeartbeatService struct {
+	heartbeatv1connect.UnimplementedHeartbeatHandler
+}
+
+func (h *HeartbeatService) ReceiveHeartbeat(_ context.Context, _ *connect.Request[heartbeatv1.HeartbeatRequest]) (*connect.Response[heartbeatv1.HeartbeatResponse], error) {
+	fmt.Println("Hearbeat received")
+	return connect.NewResponse(&heartbeatv1.HeartbeatResponse{}), nil
 }
